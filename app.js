7afc25332da21f089e153c7537622159c35b7e2a
Michael Haines <michaelhaines@Michaels-MacBook-Pro.local>
2018-10-09 21:32:38 -0700

    fixing client-side routing

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.get('*', function(req, res) {
+  res.sendfile('./build/index.html');
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
